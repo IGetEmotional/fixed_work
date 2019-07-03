961953fea151e4f45337bfc410049a3fd9a64f84
IGetEmotional <42721525+IGetEmotional@users.noreply.github.com>
2019-07-03 12:33:43 +0300

    Update task_9_fixed.rb

--- task_9_fixed.rb
+++ task_9_fixed.rb
@@ -11,17 +11,13 @@
 =end
 
 def connect_hashes(hash1, hash2)
-    k1,k2 = hash1.values, hash2.values
-   output = {}
-   sum1 = sum2 = 0
-    k1.each{|x|  sum1 = sum1+x}
-    k2.each{|x| sum2 = sum2+x}
-    output = (sum1<=sum2)? hash1.merge(hash2) : hash2.merge(hash1)
-output.delete_if{ |key,value| value < 10 }.sort_by(&:last).to_h
- end
+  k1,k2 = hash1.values, hash2.values
+  output = (k1.sum <= k2.sum)? hash1.merge(hash2) : hash2.merge(hash1)
+  output.delete_if{ |key,value| value < 10 }
+        .sort_by(&:last)
+        .to_h
+end
  
- 
- 
- p connect_hashes({ a: 2, b: 12 }, { c: 11, e: 5 })  
- p connect_hashes({ a: 13, b: 9, d: 11 }, { c: 12, a: 15 })  
- p connect_hashes({ a: 14, b: 12 }, { c: 11, a: 15 }) 
+p connect_hashes({ a: 2, b: 12 }, { c: 11, e: 5 })  
+p connect_hashes({ a: 13, b: 9, d: 11 }, { c: 12, a: 15 })  
+p connect_hashes({ a: 14, b: 12 }, { c: 11, a: 15 }) 
