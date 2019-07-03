6ec6cbba546785f7a6ac273b4255ea5e23282c95
IGetEmotional <42721525+IGetEmotional@users.noreply.github.com>
2019-07-03 12:32:21 +0300

    Update task_8_fixed.rb

--- task_8_fixed.rb
+++ task_8_fixed.rb
@@ -3,34 +3,23 @@
 входящих в inputs.
 =end
 
-
-def multiply_numbers(inputs = nil)
-    num = 1
-    check = false
-    case inputs
-    when Float, String, Integer
-             inputs = inputs.to_s
-             inputs.each_char{|y| next if !('0'..'9').include?(y)
-                        check = true
-                        num = num*(y.to_i)
-                        }  
-    when Array
-    inputs.each{|x| next if x.class != Integer
-        num = num*x
-        check = true
-         }
-    end
-num if check
- end
- 
- 
-
- 
- p multiply_numbers()                            
- p multiply_numbers('ss')  
-  p multiply_numbers('1234')                     
- p multiply_numbers('sssdd34')                   
- p multiply_numbers(2.3)                          
+def multiply_numbers(input = nil)
+  num = input.to_s
+             .gsub(/\D/,"")
+  if !num.empty? 
+    value = 1
+    num.to_i
+       .digits   
+       .each{ |x| value*=x}
+    value
+  end   
+end
+  
+p multiply_numbers()                            
+p multiply_numbers('ss')  
+p multiply_numbers('1234')                     
+p multiply_numbers('sssdd34')                   
+p multiply_numbers(2.3)                          
 p multiply_numbers([5, 6, 10]) 
 
 
