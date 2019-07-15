b26b46ea6eda303d2fcdd94c06373c566b0620d8
IGetEmotional <42721525+IGetEmotional@users.noreply.github.com>
2019-07-15 08:37:28 +0300

    Update task_5_fixed.rb

--- task_5_fixed.rb
+++ task_5_fixed.rb
@@ -14,7 +14,7 @@ require 'active_support/core_ext/numeric/time'
 
 
 def date_in_future(dayz = 0)
-  Time.now + ((dayz.is_a?(Integer))? dayz.days : 0)
+  Time.current + ((dayz.is_a?(Integer))? dayz.days : 0)
 end
 
 puts date_in_future(12)
